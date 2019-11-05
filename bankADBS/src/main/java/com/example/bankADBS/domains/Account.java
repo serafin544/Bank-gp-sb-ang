17680b479e00b2c24f47a89d01be1a54d00cdf25
serafin <serafinthedream@outlook.com>
2019-11-05 16:07:13 -0500

    setupAccount

---
+++ bankADBS/src/main/java/com/example/bankADBS/domains/Account.java
@@ -0,0 +1,62 @@
+package com.example.bankADBS.domains;
+
+public class Account {
+
+    private long id;
+    private Enum type;
+    private String nickname;
+    private int rewards;
+    private double balance;
+    private Customer customer;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Enum getType() {
+        return type;
+    }
+
+    public void setType(Enum type) {
+        this.type = type;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public int getRewards() {
+        return rewards;
+    }
+
+    public void setRewards(int rewards) {
+        this.rewards = rewards;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+
+
+}
