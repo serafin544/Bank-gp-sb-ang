6a8757495f0efa2557f63ae026ebf9f579d6b907
serafin <serafinthedream@outlook.com>
2019-11-05 14:49:07 -0500

    Starter Files

---
+++ bankADBS/src/main/java/com/example/bankADBS/BankAdbsApplication.java
@@ -0,0 +1,13 @@
+package com.example.bankADBS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAdbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankAdbsApplication.class, args);
+	}
+
+}
