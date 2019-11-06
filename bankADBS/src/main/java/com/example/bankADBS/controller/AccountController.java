b7d5761fac1df83e7283ab2adfe5d1c8df2334d1
CreatorBailey <baileym1215@gmail.com>
2019-11-06 10:07:33 -0500

    AccountShit

--- bankADBS/src/main/java/com/example/bankADBS/controller/AccountController.java
+++ bankADBS/src/main/java/com/example/bankADBS/controller/AccountController.java
@@ -1,14 +1,46 @@
 package com.example.bankADBS.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import com.example.bankADBS.domains.Account;
+import com.example.bankADBS.domains.Customer;
+import com.example.bankADBS.services.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
+@RestController
 public class AccountController {
 
-    @RequestMapping(method = RequestMethod.GET, value = "/topics")
-    public  getAllAccts(){
-        return ();
+    @Autowired
+    private AccountService accountService;
+
+    @RequestMapping(method = RequestMethod.GET, value = "/accounts")
+    public List<Account> getAllAccts(){
+        return accountService.getAllAccounts();
     }
+    @RequestMapping(method = RequestMethod.GET, value = "/accounts/{id}")
+    public Optional<Account> getById(@PathVariable Long id){
+        return accountService.getById(id);
+    }
+  /*  @RequestMapping(method = RequestMethod.GET, value = "/customers/{customerid}/accounts")
+    public Optional<Customer> getByCustId(@PathVariable Long id){
+        return accountService.getByCustId(id);
+    }*/
+
+  @RequestMapping(method = RequestMethod.POST, value = "/customers/{customerid}/accounts")
+    public void addAccount(@RequestBody Account account){accountService.addAccount(account);
+
+  }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "/accounts/{accountid}")
+public void updateAccount(@PathVariable Long id, @RequestBody Account account) {
+        accountService.updateAccount(account, id);
+    }
+    @RequestMapping(method = RequestMethod.DELETE, value = "/accounts/{accountid}")
+    public void delAccount(@RequestBody Long id){
+      accountService.delAccount(id);
+    }
+
+
 }
