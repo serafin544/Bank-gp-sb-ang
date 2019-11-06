b6104f7db56182ff5b4929f4e856b8a0702deaa8
daveeclark <dclark2016@hotmail.com>
2019-11-06 10:18:33 -0500

    really out this hoe

---
+++ bankADBS/src/main/java/com/example/bankADBS/controller/WithdrawalController.java
@@ -0,0 +1,44 @@
+package com.example.bankADBS.controller;
+
+import com.example.bankADBS.domains.Withdrawal;
+import com.example.bankADBS.services.WithdrawalService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+import java.util.Optional;
+
+public class WithdrawalController {
+
+  @Autowired
+  private WithdrawalService withdrawalService;
+
+  @RequestMapping(method = RequestMethod.GET, value = "/accounts/{accountId}/withdrawals")
+  public List<Withdrawal> getAllWithdrawals(){
+    return withdrawalService.getAllWithdrawals();
+  }
+
+  @RequestMapping(method = RequestMethod.GET, value = "/withdrawals/{withdrawalId}")
+  public Optional<Withdrawal> getWithdrawalById(String id){
+    return withdrawalService.getWithdrawalById(id);
+  }
+
+  @RequestMapping(method = RequestMethod.POST, value = "/accounts/{accountId}/withdrawals")
+  public void addWithdrawal(@RequestBody Withdrawal withdrawal){
+    withdrawalService.addWithdrawal(withdrawal);
+  }
+
+  @RequestMapping(method = RequestMethod.PUT, value = "/withdrawals/{withdrawalId}")
+  public void updateWithdrawal(@PathVariable String id, @RequestBody Withdrawal withdrawal){
+    withdrawalService.updateWithdrawal(id, withdrawal);
+  }
+
+  @RequestMapping(method = RequestMethod.DELETE, value = "/withdrawals/{withdrawalId}")
+  public void deleteWithdrawal(@PathVariable String id) {
+    withdrawalService.deleteWithdrawal(id);
+  }
+
+}
---
+++ bankADBS/src/main/java/com/example/bankADBS/repository/WithdrawalRepository.java
@@ -0,0 +1,7 @@
+package com.example.bankADBS.repository;
+
+import com.example.bankADBS.domains.Withdrawal;
+import org.springframework.data.repository.CrudRepository;
+
+public interface WithdrawalRepository extends CrudRepository<Withdrawal, String> {
+}
