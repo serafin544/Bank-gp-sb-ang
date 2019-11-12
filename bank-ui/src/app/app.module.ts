a43dd3b9c5e4c6c4e50f03d071f71bf7bd50a0df
aboyce002 <aboyce002@my.wilmu.edu>
2019-11-12 15:37:48 -0500

    more ui stuff

--- bank-ui/src/app/app.module.ts
+++ bank-ui/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -24,7 +25,9 @@ import { BankService } from './services/bank.service';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule, 
+    ReactiveFormsModule
   ],
   providers: [BankService],
   bootstrap: [AppComponent]
