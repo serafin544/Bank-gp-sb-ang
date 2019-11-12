d8f48a6911302051f921fb4de764513e3f226778
aboyce002 <aboyce002@my.wilmu.edu>
2019-11-12 13:58:32 -0500

    added basic ui stuff

--- bank-ui/src/app/app.module.ts
+++ bank-ui/src/app/app.module.ts
@@ -3,16 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { HomeComponent } from './components/home/home.component';
+import { AccountComponent } from './components/account/account.component';
+import { DepositComponent } from './components/deposit/deposit.component';
+import { WithdrawalComponent } from './components/withdrawal/withdrawal.component';
+import { HttpClientModule } from '@angular/common/http';
+
+import { BankService } from './services/bank.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    HomeComponent,
+    AccountComponent,
+    DepositComponent,
+    WithdrawalComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [BankService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
